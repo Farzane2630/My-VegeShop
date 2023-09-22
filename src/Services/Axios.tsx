3b76d74cc8771cccbc45051e7a15c9fa1ee55fc3
Farzaneh <farzanekazemi8517@gmail.com>
2023-09-22 15:20:22 +0330

    check database address

--- src/Services/Axios.tsx
+++ src/Services/Axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const BASE_URL = axios.create({
-   baseURL: "https://poetic-cactus-02e4e8.netlify.app",
+   baseURL: "https://charming-florentine-18c793.netlify.app/db.json/",
    headers: {
       "Content-Type": 'application/json'
    }
