a683c318a3165dbe0d053965a92ddc2c24efd247
Farzaneh <farzanekazemi8517@gmail.com>
2024-08-12 20:39:01 +0330

    update BASE_URL

--- src/Services/Axios.tsx
+++ src/Services/Axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const BASE_URL = axios.create({
-   baseURL: "http://localhost:3000",
+   baseURL: "https://vegan-shop-db.vercel.app/",
    headers: {
       "Content-Type": 'application/json'
    }
